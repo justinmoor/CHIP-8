1534f48b6890b44fcb449d9e05ef37ca2e2904d6
Justin <justin-804@hotmail.com>
2019-05-16 11:40:28 +0200

    whitespace fix

--- system/chip8.go
+++ system/chip8.go
@@ -7,38 +7,38 @@ import (
 )
 
 var (
-	soundTimer 	int
-	delayTimer	int
+	soundTimer int
+	delayTimer int
 )
 
-type CHIP8 struct{
+type CHIP8 struct {
 	cpu
-	gfx			[64 * 32]byte	// display
-	key			[16]byte		// current key state
-	DrawFlag	bool
+	gfx      [64 * 32]byte // display
+	key      [16]byte      // current key state
+	DrawFlag bool
 }
 
 type cpu struct {
-	opcode 	uint16		// opcode is 2 bytes
-	memory 	[4096]byte
-	v		[16]byte	// CPU registers
-	i		uint16		// index register
-	pc		uint16		// program counter
-	stack	[16]uint16
-	sp		uint16		// stackpointer
+	opcode uint16 // opcode is 2 bytes
+	memory [4096]byte
+	v      [16]byte // CPU registers
+	i      uint16   // index register
+	pc     uint16   // program counter
+	stack  [16]uint16
+	sp     uint16 // stackpointer
 }
 
-func (c *CHIP8) Initialize(){
-	c.pc		= 0x200 // = 512: initial point where a program will start
-	c.opcode	= 0x00
-	c.i			= 0x00
-	c.sp		= 0x00
-	c.DrawFlag	= true
+func (c *CHIP8) Initialize() {
+	c.pc = 0x200 // = 512: initial point where a program will start
+	c.opcode = 0x00
+	c.i = 0x00
+	c.sp = 0x00
+	c.DrawFlag = true
 
 	// clear all the memory
-	c.memory	= [4096]byte{}
-	c.v			= [16]byte{}
-	c.stack		= [16]uint16{}
+	c.memory = [4096]byte{}
+	c.v = [16]byte{}
+	c.stack = [16]uint16{}
 
 	// load font into memory
 	for i := 0; i < 0x50; i++ {
@@ -50,19 +50,11 @@ func (c *CHIP8) Initialize(){
 // decode opcode
 // execude opcode
 // update timers
-func (c *CHIP8) Cycle(){
-	//c.opcode = uint16((c.memory[c.pc] << 8) | c.memory[c.pc + 1])
-	// compiler optimization:
+func (c *CHIP8) Cycle() {
 	c.opcode = uint16(c.memory[c.pc])
 	c.opcode <<= 8
-	c.opcode |= uint16(c.memory[c.pc + 1])
-	c.pc += 2
-	/*
-	int a = opcode & 0xF000;
-	int b = opcode & 0x0FFF;
-	if(a == 0x1000)
-	   doJump(b);
-	*/
+	c.opcode |= uint16(c.memory[c.pc+1])
+
 	switch c.opcode & 0xF000 {
 	case 0x0000:
 		switch c.opcode & 0x000F {
@@ -83,17 +75,17 @@ func (c *CHIP8) Cycle(){
 		c.pc = c.opcode & 0x0FFF
 		break
 	case 0x3000:
-		if uint16(c.v[c.opcode & 0x0F00]) == c.opcode & 0x00FF {
+		if uint16(c.v[c.opcode&0x0F00]) == c.opcode&0x00FF {
 			c.pc += 2
 		}
 		break
 	case 0x0004:
-		if c.v[(c.opcode & 0x00F0) >> 4] > (0xFF - c.v[(c.opcode & 0x0F00) >> 8]) {
+		if c.v[(c.opcode&0x00F0)>>4] > (0xFF - c.v[(c.opcode&0x0F00)>>8]) {
 			c.v[0xF] = 1
 		} else {
 			c.v[0xF] = 0
 		}
-		c.v[(c.opcode & 0x0F00) >> 8] += c.v[(c.opcode & 0x00F0) >> 4]
+		c.v[(c.opcode&0x0F00)>>8] += c.v[(c.opcode&0x00F0)>>4]
 		c.pc += 2
 		break
 	case 0xA000:
@@ -101,20 +93,20 @@ func (c *CHIP8) Cycle(){
 		c.pc += 2
 		break
 	case 0x0033:
-		c.memory[c.i]		= c.v[(c.opcode & 0x0F00) >> 8] / 100
-		c.memory[c.i + 1]	= (c.v[(c.opcode & 0x0F00) >> 8] / 10) % 10
-		c.memory[c.i + 2]	= (c.v[(c.opcode & 0x0F00) >> 8] % 100) % 10
+		c.memory[c.i] = c.v[(c.opcode&0x0F00)>>8] / 100
+		c.memory[c.i+1] = (c.v[(c.opcode&0x0F00)>>8] / 10) % 10
+		c.memory[c.i+2] = (c.v[(c.opcode&0x0F00)>>8] % 100) % 10
 		break
 	default:
 		fmt.Println("Unsupported")
 	}
 }
 
-func (c *CHIP8) clearDisplay(){
+func (c *CHIP8) clearDisplay() {
 	c.gfx = [64 * 32]byte{}
 }
 
-func (c *CHIP8) Load(romName string) error{
+func (c *CHIP8) Load(romName string) error {
 	rom, err := os.Open(romName)
 	if err != nil {
 		return err
@@ -133,8 +125,9 @@ func (c *CHIP8) Load(romName string) error{
 		return err
 	}
 
+	// read ROM into memory
 	for i := 0; i < len(bytes); i++ {
-		c.memory[i + 0x200] = bytes[i]
+		c.memory[i+0x200] = bytes[i]
 		hex := fmt.Sprintf("%x", c.memory[i+0x200])
 		fmt.Printf("%s\n", hex)
 	}
