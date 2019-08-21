17a1849da59afe6f79c5fb8feedc7ef482196ec4
Justin <justin-804@hotmail.com>
2019-08-21 19:57:03 +0200

    remove gui and update wasm build. Now fully works, still needs performance finetuning.

---
+++ cmd/server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Println("Server listening on port 8000...")
+	log.Fatal(http.ListenAndServe(":8000", http.FileServer(http.Dir("../static"))))
+}
---
+++ cmd/wasm/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"CHIP-8/chip8"
+	"syscall/js"
+)
+
+var (
+	window, doc, body, canvas, ctx, beep js.Value
+	width                                float64 = 64
+	height                               float64 = 32
+	c                                    *chip8.CHIP8
+)
+
+const scale = 16
+
+func setup() {
+	window = js.Global()
+	doc = window.Get("document")
+	body = doc.Get("body")
+
+	height = window.Get("innerHeight").Float()
+	width = window.Get("innerWidth").Float()
+
+	canvas = doc.Call("createElement", "canvas")
+	canvas.Set("height", height)
+	canvas.Set("width", width)
+	body.Call("appendChild", canvas)
+
+	ctx = canvas.Call("getContext", "2d")
+	ctx.Set("fillStyle", "black")
+	ctx.Call("scale", scale, scale)
+
+	doc.Call("addEventListener", "keydown", keyEvent)
+}
+
+var keyEvent = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	e := args[0]
+	keyPress := e.Get("key")
+
+	if key, ok := chip8.KeyMap[keyPress.String()]; ok {
+		c.SendKeyPress(key)
+	}
+
+	return nil
+})
+
+func main() {
+	setup()
+	c = chip8.New()
+
+	if err := c.LoadRomHTTP("http://localhost:8000/roms/PONG1"); err != nil {
+		panic(err)
+	}
+
+	var renderer js.Func
+	renderer = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		ctx.Call("clearRect", 0, 0, width, height)
+		c.Cycle()
+		if c.DrawFlag {
+			for x := 0; x < chip8.Width; x++ {
+				for y := 0; y < chip8.Height; y++ {
+					if c.Gfx[x][y] == 1 {
+						ctx.Call("fillRect", x, y, 1, 1)
+					}
+				}
+			}
+		}
+
+		window.Call("requestAnimationFrame", renderer)
+		return nil
+	})
+
+	window.Call("requestAnimationFrame", renderer)
+
+	select {}
+}
